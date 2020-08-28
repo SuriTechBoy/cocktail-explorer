092c53aa515ad0f6c90fa3d1fbf87f570cb75f67
SuriTechBoy <diegofranco711@gmail.com>
2020-08-28 08:54:54 -0300

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
-import Cocktails from './components/Cocktails'
+import CocktailCategoriesContainer from './components/CocktailCategoriesContainer';
+import { Route } from 'react-router-dom'
+import CocktailImages from './components/CocktailImages';
 
 function App() {
   return (
     <div className="App">
       <main className="App-header">
-        <Cocktails />
+        <Route exact path='/' component={CocktailCategoriesContainer} />
+        <Route path='/cocktails/:category' component={CocktailImages} />
       </main>
     </div>
   );
